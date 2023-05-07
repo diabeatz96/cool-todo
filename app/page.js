b3bfed5f10d68888d27fa6872828be0e21f755d5
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2023-05-07 10:37:46 -0400

    Background Added, Footer Improved, Layout Improved, Added Cool Fun Text!

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold text-center">Hello World</h1>
+    <main className="p-24 ">
+      <h1 className="text-6xl text-shadow font-bold text-center text-warning ">Hello World</h1>
     </main>
   )
 }
