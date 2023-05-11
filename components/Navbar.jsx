7af4819009428606cdb01b9a49fa338feb76bd4d
AladdinOmar61 <Aladdin.Omar61@gmail.com>
2023-05-11 17:58:35 -0400

    register now actually works
    register function now works properly, adds user to supabase table and username field is also used as slug

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 import NavButton from "./NavButton";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar bg-accent flex gap-2 pt-10 zigzag">
-            <NavButton link="/" name="Home" />
-            <NavButton link="/:name" name="My Account" />
-            <NavButton link="/contact" name="User List" />
-            <NavButton link="/login" name="Login" />
-            <NavButton link="/register" name="Register" />
-        </nav>
-    )
-}
+  return (
+    <nav className="navbar bg-accent flex gap-2 pt-10 zigzag">
+      <NavButton link="/" name="Home" />
+      <NavButton link="/user" name="My Account" />
+      <NavButton link="/contact" name="User List" />
+      <NavButton link="/login" name="Login" />
+      <NavButton link="/register" name="Register" />
+    </nav>
+  );
+};
 
 export default Navbar;
--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,90 +1,116 @@
-"use client"
+"use client";
 
-import { registerUser } from '@/utils/auth'
-import { useRouter } from 'next/navigation'
-import { useReducer, useState } from 'react'
+import { registerUser } from "@/utils/auth";
+import { useRouter } from "next/navigation";
+import { useReducer, useState } from "react";
 
 const RegisterForm = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    function reducer(state, action) {
-        switch (action.type) {
-            case "email":
-            case "name":
-            case "password":
-            case "slug":
-                return {...state, [action.type]: action.value};
-            case "loading":
-                return {...state, loading: action.loading};
-            case "response":
-                return {...state, response: action.response};
-        }
-
-        throw Error("Unknown action." + action.type);
+  function reducer(state, action) {
+    switch (action.type) {
+      case "email":
+      case "name":
+      case "password":
+      case "slug":
+        return { ...state, [action.type]: action.value };
+      case "loading":
+        return { ...state, loading: action.loading };
+      case "response":
+        return { ...state, response: action.response };
     }
 
-    const initialState = {
-        email: "",
-        name: "",
-        password: "",
-        slug: "",
-        response: "",
-        loading: false,
-    };
+    throw Error("Unknown action." + action.type);
+  }
 
-    const [username, setUsername] = useState('')
-    // const [password, setPassword] = useState('')
-    const [passwordConfirm, setPasswordConfirm] = useState('')
-    // const [email, setEmail] = useState('')
-    const [emailConfirm, setEmailConfirm] = useState('')
-    const [error, setError] = useState('')
+  const initialState = {
+    email: "",
+    name: "",
+    password: "",
+    slug: "",
+    response: "",
+    loading: false,
+  };
 
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const { email, name, slug=name, password, response, loading } = state;
+  const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  // const [email, setEmail] = useState('')
+  const [emailConfirm, setEmailConfirm] = useState("");
+  const [error, setError] = useState("");
 
-    const register = async (e) => {
-        dispatch({ type: "loading", loading: true });
-        e.preventDefault();
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { email, name, slug = name, password, response, loading } = state;
 
-        const response = await registerUser(name, email, password, slug);
-        dispatch({ type: "response", response });
-        dispatch({ type: "loading", loading: false });
-        if(response?.success) {
-            setTimeout(() => {
-                router.push("/login");
-            }, 3000);
-        }
-    };
+  const register = async (e) => {
+    dispatch({ type: "loading", loading: true });
+    e.preventDefault();
 
-    return (
-        
-        <div className="h-90v flex items-center justify-center">
-            {response && (
-            <div className={`${response.success ? "bg-green-200 broder-2 border-green-800 text-green-800" : 
-            "bg-red-200 border-2 border-red-800 text-red-800"} py-2 px-5 my-10 text-center`}>
-                <span className="font-bold">
-                    {response.success ? `Success ${response.message ? `: ` : ``}` : "Failure: "}
+    const response = await registerUser(name, email, password, name);
+    dispatch({ type: "response", response });
+    dispatch({ type: "loading", loading: false });
+    if (response?.success) {
+      setTimeout(() => {
+        router.push("/login");
+      }, 3000);
+    }
+  };
+
+  return (
+    <div className="h-90v flex items-center justify-center">
+      {response && (
+        <div
+          className={`${
+            response.success
+              ? "bg-green-200 broder-2 border-green-800 text-green-800"
+              : "bg-red-200 border-2 border-red-800 text-red-800"
+          } py-2 px-5 my-10 text-center`}
+        >
+          <span className="font-bold">
+            {response.success
+              ? `Success ${response.message ? `: ` : ``}`
+              : "Failure: "}
+          </span>
+          {response.message}
+        </div>
+      )}
+      <section className="card bg-register-bg2 background-shadow bg-cover w-96 text-slate-900">
+        <div className="card-body items-center ">
+          <form className="form">
+            <div className="form-control w-full max-w-xs">
+              <label htmlFor="username" className="label">
+                <span className="label-text badge-primary p-2 rounded-lg text-black">
+                  What is your username?
                 </span>
-                {response.message}
+              </label>
+              <input
+                type="text"
+                placeholder="name"
+                name="name"
+                className=" text-white input input-primary input-bordered w-full max-w-xs"
+                value={name}
+                onChange={(e) => {
+                  dispatch({ type: e.target.name, value: e.target.value });
+                }}
+              />
             </div>
-        )}
-            <section className = "card bg-register-bg2 background-shadow bg-cover w-96 text-slate-900">
-            <div className="card-body items-center ">
-        <form className="form">
             <div className="form-control w-full max-w-xs">
-                <label htmlFor="username" className="label">
-                    <span className="label-text badge-primary p-2 rounded-lg text-black">What is your username?</span>
-                </label>
-                <input type="text" placeholder="name" name="name" className=" text-white input input-primary input-bordered w-full max-w-xs" 
-                value={name} onChange={((e) => {dispatch({ type: e.target.name, value: e.target.value })})} />
-            </div>
-            <div className="form-control w-full max-w-xs">
-                <label htmlFor="password" className="label">
-                    <span className="label-text badge-secondary p-2 rounded-lg text-black">What is your password?</span>
-                </label>
-                <input type="password" placeholder="Password" name="password" className=" text-white input input-primary input-bordered w-full max-w-xs" 
-                value={password} onChange={((e) => {dispatch({ type: e.target.name, value: e.target.value })})}  />
-                {console.log(password.length)}
+              <label htmlFor="password" className="label">
+                <span className="label-text badge-secondary p-2 rounded-lg text-black">
+                  What is your password?
+                </span>
+              </label>
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                className=" text-white input input-primary input-bordered w-full max-w-xs"
+                value={password}
+                onChange={(e) => {
+                  dispatch({ type: e.target.name, value: e.target.value });
+                }}
+              />
+              {console.log(password.length)}
             </div>
             {/* <div className="form-control w-full max-w-xs">
                 <label htmlFor="password" className="label">
@@ -93,11 +119,21 @@ const RegisterForm = () => {
                 <input type="password" placeholder="Password" name="password" className=" text-white input input-primary input-bordered w-full max-w-xs" />
             </div> */}
             <div className="form-control w-full max-w-xs">
-                <label htmlFor="email" className="label">
-                    <span className="label-text badge-warning p-2 rounded-lg text-black">What is your email?</span>
-                </label>
-                <input type="email" placeholder="Email" name="email" className=" text-white input input-primary input-bordered w-full max-w-xs"
-                value={email} onChange={((e) => {dispatch({ type: e.target.name, value: e.target.value })})} />
+              <label htmlFor="email" className="label">
+                <span className="label-text badge-warning p-2 rounded-lg text-black">
+                  What is your email?
+                </span>
+              </label>
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                className=" text-white input input-primary input-bordered w-full max-w-xs"
+                value={email}
+                onChange={(e) => {
+                  dispatch({ type: e.target.name, value: e.target.value });
+                }}
+              />
             </div>
             {/* <div className="form-control w-full max-w-xs">
                 <label htmlFor="email" className="label">
@@ -105,12 +141,17 @@ const RegisterForm = () => {
                 </label>
                 <input type="email" placeholder="Email" name="email" className=" text-white input input-primary input-bordered w-full max-w-xs" />
             </div> */}
-            <button className="btn mt-4 btn-primary w-full max-w-xs" onClick={register}>Register</button>
-            </form>
-            </div>
-            </section>
-            </div>
-    )
-}
+            <button
+              className="btn mt-4 btn-primary w-full max-w-xs"
+              onClick={register}
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default RegisterForm;
