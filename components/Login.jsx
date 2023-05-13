5edef87751085e1501745b8c66ad69ba86c70475
AladdinOmar61 <Aladdin.Omar61@gmail.com>
2023-05-12 21:29:11 -0400

    Login Page WIP
    Made progress to login page, getting error on click to submit

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,45 +1,117 @@
-"use client"
+"use client";
 
-import React, { useState } from "react";
+import { loginUser } from "@/utils/auth";
+import { useRouter } from "next/navigation";
+import React, { useReducer, useState } from "react";
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [login, setLogin] = useState(false);
-    const [error, setError] = useState(false);
+  const router = useRouter();
 
-    function onChangeUsernameHandler(event) {
-        setUsername(event.target.value);
+  function reducer(state, action) {
+    switch (action.type) {
+      case "email":
+      case "password":
+        return { ...state, [action.type]: action.value };
+      case "loading":
+        return { ...state, loading: action.value };
+      case "response":
+        return { ...state, response: action.value };
     }
 
-    function onChangePasswordHandler(event) {
-        setPassword(event.target.value);
+    throw Error("Unknown action." + action.type);
+  }
+
+  const initialState = {
+    email: "",
+    userPassword: "",
+    response: "",
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { email, userPassword, response, loading } = state;
+
+  //   const [username, setUsername] = useState("");
+  //   const [password, setPassword] = useState("");
+  //   const [login, setLogin] = useState(false);
+  //   const [error, setError] = useState(false);
+
+  const userLogin = async (e) => {
+    dispatch({ type: "loading", value: true });
+    dispatch({ type: "response", value: null });
+    e.preventDefault();
+
+    const response = await loginUser(email, userPassword);
+
+    dispatch({ type: "response", value: response });
+    dispatch({ type: "loading", value: false });
+    if (response?.success) {
+      setTimeout(() => {
+        router.replace("/user");
+      }, 3000);
     }
-    
-    return (
-        <section className = "card w-96 bg-.login-bg bg-cover bg-no-repeat shadow-xl text-slate-900">
-            <div className="card-body">
-                <h2 className="card-title text-white">Login</h2>
-                <form className="form">
-                    <div className="form-control w-full max-w-xs">
-                        <label htmlFor="username" className="label">
-                            <span className="label-text text-white">Username</span>
-                        </label>
-                        <input onChange={onChangeUsernameHandler} type="text" placeholder="Username" name="username" className=" text-white input input-primary input-bordered w-full max-w-xs" />
-                    </div>
-                    <div className="form-control w-full max-w-xs">
-                        <label htmlFor="password" className="label">
-                            <span className="label-text text-white">Password</span>
-                        </label>
-                        <input onChange={onChangePasswordHandler} type="password" placeholder="Password" name="password" className=" text-white input input-primary input-bordered w-full max-w-xs" />
-                    </div>
-                    <div className="card-actions justify-end pt-3">
-                        <button onClick={() => setLogin(true)} className="btn btn-primary">Login</button>
-                    </div>
-                </form>
-            </div>
-        </section>
-    );
-}
+  };
+
+  return (
+    <section className="card w-96 bg-.login-bg bg-cover bg-no-repeat shadow-xl text-slate-900">
+      {response && (
+        <div
+          className={`${
+            response.success
+              ? "bg-green-200 broder-2 border-green-800 text-green-800"
+              : "bg-red-200 border-2 border-red-800 text-red-800"
+          } py-2 px-5 my-10 text-center`}
+        >
+          <span className="font-bold">
+            {response.success
+              ? `Success ${response.message ? `: ` : ``}`
+              : "Failure: "}
+          </span>
+          {response.message}
+        </div>
+      )}
+      <div className="card-body">
+        <h2 className="card-title text-white">Login</h2>
+        <form className="form">
+          <div className="form-control w-full max-w-xs">
+            <label htmlFor="username" className="label">
+              <span className="label-text text-white">Username</span>
+            </label>
+            <input
+              onChange={(e) => {
+                dispatch({ type: e.target.name, value: e.target.value });
+              }}
+              type="text"
+              placeholder="Username"
+              name="email"
+              className=" text-white input input-primary input-bordered w-full max-w-xs"
+              value={email}
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label htmlFor="password" className="label">
+              <span className="label-text text-white">Password</span>
+            </label>
+            <input
+              onChange={(e) => {
+                dispatch({ type: e.target.name, value: e.target.value });
+              }}
+              type="password"
+              placeholder="Password"
+              name="password"
+              className=" text-white input input-primary input-bordered w-full max-w-xs"
+              password={userPassword}
+            />
+          </div>
+          <div className="card-actions justify-end pt-3">
+            <button onClick={userLogin} className="btn btn-primary">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default Login;
--- utils/auth.js
+++ utils/auth.js
@@ -1,78 +1,91 @@
 import supabase from "./supabase";
 
-
 const registerUser = async (name, email, password, slug) => {
-    const { data, error } = await supabase
+  const { data, error } = await supabase
     .from("users")
     .select("*")
     .eq("slug", slug);
-    if(error) {
-        return {
-            success: false,
-            message: error.message,
-        };
-    }
-    if(data.length > 0) {
-        return {
-            success: false,
-            message: "Slug already exists",
-        };
-    }
+  if (error) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+  if (data.length > 0) {
+    return {
+      success: false,
+      message: "Slug already exists",
+    };
+  }
 
-    const authResponse = await supabase.auth.signUp({
-        email,
-        password,
-    });
+  const authResponse = await supabase.auth.signUp({
+    email,
+    password,
+  });
 
-    if(authResponse.error) {
-        return {
-            success: false,
-            message: authResponse.error.message,
-        };
+  if (authResponse.error) {
+    return {
+      success: false,
+      message: authResponse.error.message,
+    };
+  }
+
+  if (authResponse.data.user) {
+    const addMetaResponse = await supabase
+      .from("users")
+      .insert([{ email, name, slug }]);
+    console.log(authResponse.data.user);
+    if (addMetaResponse.error) {
+      return {
+        success: false,
+        message: addMetaResponse.error.message,
+      };
     }
+    return {
+      success: true,
+      message: "Registration complete! Redirecting...",
+      ...addMetaResponse.data,
+    };
+  }
 
-    if(authResponse.data.user) {
-        const addMetaResponse = await supabase
-        .from('users')
-        .insert([{ email, name, slug }]);
-        console.log(authResponse.data.user)
-        if(addMetaResponse.error) {
-            return {
-                success: false,
-                message: addMetaResponse.error.message,
-            };
-        }
-        return {
-            success: true,
-            message: "Registration complete! Redirecting..." ,
-            ...addMetaResponse.data,
-        }
+  return {
+    success: false,
+    message: "An unknown error has occurred!!",
+  };
+};
+
+const loginUser = async (email, password) => {
+  const authResponse = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (authResponse.error) {
+    return {
+      success: false,
+      message: authResponse.error,
+    };
+  }
+
+  if (authResponse.data.user) {
+    const meta = await supabase.from("profile").select("*").eq("email", email);
+
+    if (meta.error) {
+      return {
+        success: false,
+        message: meta.error,
+      };
     }
 
     return {
-        success: false,
-        message: "An unknown error has occurred!!"
+      ...authResponse,
+      meta,
+      success: true,
     };
-}
+  }
+  return {
+    success: false,
+    message: "An unknown error has occurred",
+  };
+};
 
-const loginUser = async (name, password) => {
-    const authResponse = await supabase.auth.signInWithPassword({
-        email,
-        password
-    })
-    if(authResponse.error) {
-        return {
-            success: false,
-            message: authResponse.error,
-        };
-    }
-
-    if(authResponse.data.user) {
-        const meta = await supabase
-        .from("profile")
-        .select("*")
-        .eq("name", name)
-    }
-}
-
-export {registerUser};
+export { registerUser, loginUser };
